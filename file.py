8c56d3b44ed7ea934ef8d7b1b892dc86a4e2502c
alexandru.alexandru <alexandru.alexandru@mta.ro>
2021-12-19 18:38:58 +0200

    first commit!

--- file.py
+++ file.py
@@ -11,7 +11,22 @@ def function1():
     for i in range(10):
         print("Abc\n")
 
-main()
+def function2():
+    for i in range(10):
+        print("Abc\n")
 
-def function213():
-    print("Hello")
+def function3():
+    for i in range(10):
+        print("Abc\n")
+
+def function4():
+    for i in range(10000):
+        print("Ana are mere si pere!")
+
+def function5():
+    print("Hello from the otherside")
+
+def function6():
+    print(1+2+3+4+5+6+7)
+
+main()
