75b9be275c7f90bac16a67c04e272f4a173f18f0
alexandru.alexandru <alexandru.alexandru@mta.ro>
2022-02-28 16:59:52 +0200

    New commit

--- file2.js
+++ file2.js
@@ -1,8 +1,22 @@
 alert("Hello");
 
-function()
-{
+function() {
 
 }
 
-alert("Hello")
+if (message !== "") {
+    axios
+        .post("http://localhost:8080/workspace/push", {
+            link: localStorage.getItem('githublink'),
+            message: message,
+            branch: "master",
+            directory: localRepo
+        })
+        .then((response) => {
+            if (response.data === "Error") {
+                alertProperties(true, "error", "Error", "Push can't be made!");
+            } else {
+                alertProperties(true, "success", "Success", "Successfully push!");
+            }
+        });
+}
