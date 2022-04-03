5f7306513cc9f554e5012405d65bcb0fc2ce98d4
alexandru.alexandru <alexandru.alexandru@mta.ro>
2022-04-03 17:45:12 +0300

    New

--- file1.c
+++ file1.c
@@ -29,6 +29,42 @@ void func() {
     }
 }
 
+void new_func1()
+{
+    int counter = 0;
+    for (int i=0;i<100;i++)
+    {
+        counter = counter + 1;
+    }
+    printf("%d",counter);
+}
+
+void func() {
+    printf("Hello");
+    int suma = 0;
+    for(int i = 0;i<100;i++) {
+        suma = suma = i;
+    }
+}
+
+
+void func() {
+    printf("Hello");
+    int suma = 0;
+    for(int i = 0;i<100;i++) {
+        suma = suma = i;
+    }
+}
+
+
+void func() {
+    printf("Hello");
+    int suma = 0;
+    for(int i = 0;i<100;i++) {
+        suma = suma = i;
+    }
+}
+
 int main()
 {
     printf("Hello world!\n");
